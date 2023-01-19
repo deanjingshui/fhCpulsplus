897eee95830934f0d06f072d15576eac13dcdcb9
deanjingshui <fenghao.0721@bytedance.com>
2023-01-19 14:19:38 +0800

    [chenro]update array

--- cherno/31_array/size.cc
+++ cherno/31_array/size.cc
@@ -4,4 +4,24 @@ class Entity
 {
 public:
     int* example = new int[5];
+
+
+    Entity()
+    {
+        int a[5];  // 在栈中分配的数组
+        sizeof(a);  // 得到数组占了多少字节, 4*5 = 20
+        int count = sizeof(a) / sizeof(int);  // 数组里有多少元素，除以数组类型的大小 20/4 = 5
+
+        sizeof(example);  // 得到指针的大小
+
+        for (int i = 0; i < 5; i++)
+            example[i] = 2;
+    }
+};
+
+int main()
+{
+    Entity e;
+
+    std::cin.get();
 }
